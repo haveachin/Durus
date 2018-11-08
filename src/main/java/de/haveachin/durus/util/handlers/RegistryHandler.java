67f129ab9a0edea561b78c3d0de9955ff576c091
Haveachin <dev.H4341@gmail.com>
2018-11-08 13:09:49 +0100

    Frist attempt

--- src/main/java/de/haveachin/durus/util/handlers/RegistryHandler.java
+++ src/main/java/de/haveachin/durus/util/handlers/RegistryHandler.java
@@ -1,16 +1,23 @@
 package de.haveachin.durus.util.handlers;
 
-import java.util.List;
-
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber
 public class RegistryHandler
 {
-	
+	@SubscribeEvent
+	public static void onWorldLoad(WorldEvent.Load event)
+	{
+		World w = event.getWorld();
+		
+		String log = new StringBuilder()
+				.append("World difficulty: ")
+				.append(w.getDifficulty().toString())
+				.toString();
+		
+		System.console().writer().println(log);
+	}
 }
