bc8556be26c9febc3dd6a1be62522916be66c451
Haveachin <dev.H4341@gmail.com>
2018-11-08 16:20:54 +0100

    Doesn't crash

--- src/main/java/de/haveachin/durus/util/handlers/RegistryHandler.java
+++ src/main/java/de/haveachin/durus/util/handlers/RegistryHandler.java
@@ -1,37 +1,31 @@
 package de.haveachin.durus.util.handlers;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
-import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 @EventBusSubscriber
 public class RegistryHandler
 {
 	@SubscribeEvent
-	public static void onWorldLoad(WorldEvent.Load event)
+	public void onEntityJoinWorld(EntityJoinWorldEvent event)
 	{
-		World w = event.getWorld();
+		World w = event.getEntity().getEntityWorld();
+		
+		if (w == null)
+		{
+			System.out.println("The world is null!");
+			return;
+		}
 		
 		String log = new StringBuilder()
 				.append("World difficulty: ")
 				.append(w.getDifficulty().toString())
 				.toString();
 		
-		System.console().writer().println(log);
+		System.out.println(log);
 	}
 }
-
-/*
-	Error:
-	
-	java.lang.NullPointerException: Exception in server tick loop
-			at de.haveachin.durus.util.handlers.RegistryHandler.onWorldLoad(RegistryHandler.java:21)
-			at net.minecraftforge.fml.common.eventhandler.ASMEventHandler_4_RegistryHandler_onWorldLoad_Load.invoke(.dynamic)
-			at net.minecraftforge.fml.common.eventhandler.ASMEventHandler.invoke(ASMEventHandler.java:90)
-			at net.minecraftforge.fml.common.eventhandler.EventBus.post(EventBus.java:182)
-			at net.minecraft.server.integrated.IntegratedServer.loadAllWorlds(IntegratedServer.java:133)
-			at net.minecraft.server.integrated.IntegratedServer.init(IntegratedServer.java:160)
-			at net.minecraft.server.MinecraftServer.run(MinecraftServer.java:552)
-			at java.lang.Thread.run(Unknown Source)
-*/
